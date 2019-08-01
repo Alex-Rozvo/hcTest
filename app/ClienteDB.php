e57f7aa05b1f898dbfd8762a197e89261e0a1996
Alex <kuugal@live.com>
2019-08-01 13:02:26 -0500

    Vue

---
+++ app/ClienteDB.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class ClienteDB extends Model
+{
+    public $table = "ClienteDB";
+}
---
+++ app/Health.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class Health extends Model
+{
+    public $table = "Health";
+}
